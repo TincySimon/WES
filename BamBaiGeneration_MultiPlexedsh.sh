2f105569c5d48fe6de40177017feeca712ad770a
Tincy Simon <TincySimon@users.noreply.github.com>
2021-06-01 12:43:02 +0200

    Add files via upload

---
+++ BamBaiGeneration_MultiPlexedsh.sh
@@ -0,0 +1,229 @@
+#generating bam and bai files from fastq files for all samples in the PNET category which were ran in L1 and L2
+
+LANE = ["L1", "L2"]
+
+SAMPLES = ["PNET19N", "PNET19P", "PNET21N", "PNET21P", "PNET23N", "PNET23P", "PNET24N", "PNET24P", "PNET9N", "PNET9P"] 
+
+rule all:
+     input: 
+        expand("output/bam/{sample}/{sample}_{lane}.bam", sample=SAMPLES, lane=LANE), 
+	expand("output/bam/{sample}/{sample}_merged.bam", sample=SAMPLES),
+	expand("output/bam/{sample}/{sample}_sorted.bam", sample=SAMPLES),
+	expand("output/bam/{sample}/{sample}_dedup.bam", sample=SAMPLES),
+	expand("output/bam/{sample}/{sample}_dedup.bam.bai", sample=SAMPLES),
+	expand("output/bam/{sample}/{sample}_sorted.bam.bai", sample=SAMPLES),
+	expand("output/realin_inter/ReAlignerTaretCreator_{sample}.intervals", sample=SAMPLES),
+	expand("output/bam/{sample}/{sample}_realigned.bam", sample=SAMPLES),
+	expand("output/BQSR/{sample}/{sample}_CovarTable_Recal.table", sample=SAMPLES),
+	expand("output/bam/{sample}/{sample}_recalibrated.bam", sample=SAMPLES)
+
+rule BWA_Align:
+    input: 
+        READONE = "incoming/{sample}/{sample}_{lane}_R1.fastq.gz",
+        READTWO = "incoming/{sample}/{sample}_{lane}_R2.fastq.gz"
+    
+    params:
+        #RG= "@RG\tID:{lane}\tSM:{sample}\tPL:illumina\tLB:AgilentSureSelect\tPU:2000",    
+        BWAREF = "/fast/projects/cubit/current/static_data/precomputed/BWA/0.7.15/GRCh37/hs37d5/hs37d5.fa" #indexed?
+    
+    output: "output/bam/{sample}/{sample}_{lane}.bam"
+
+    log: "log/BWA/{sample}/{sample}_{lane}.log"
+
+    benchmark: "benchmarks/{sample}_{lane}_BWA_Align_benchmark.txt"
+
+    shell: 
+      r"""  
+      module load BWA/0.7.15-foss-2015a 
+      module load SAMtools/1.3.1-foss-2015a
+             
+      (bwa mem -M \
+	  -R "@RG\tID:{wildcards.lane}\tSM:{wildcards.sample}\tPL:illumina\tLB:AgilentSureSelect\tPU:2000" \
+	  {params.BWAREF} \
+	  {input.READONE} {input.READTWO} \
+      | samtools view -b -S -o {output}) &> {log}
+      """
+
+def generate_mergeInput(wildcards):
+    return ["output/bam/{sample}/{sample}_{lane}.bam".format(sample = wildcards.sample, lane = current_lane) for current_lane in LANE]
+
+rule MergeBam:
+   input: 
+       generate_mergeInput
+
+   output: "output/bam/{sample}/{sample}_merged.bam"
+   
+   log: "log/bam_merge/{sample}/{sample}_merged.log"
+
+   benchmark: "benchmarks/{sample}_MergeBam_benchmark.txt"
+
+   shell:
+     r"""
+     INPUTS=$(echo {input} | sed -e 's/output/ I=output/g')
+     	
+     (java -jar /fast/users/stincy/scratch/miniconda/pkgs/picard-2.11.0-py35_0/share/picard-2.11.0-0/picard.jar MergeSamFiles \
+      $INPUTS \
+      O={output}) &> {log}
+     """
+
+rule bam_sort:
+    input: "output/bam/{sample}/{sample}_merged.bam"
+
+    output: "output/bam/{sample}/{sample}_sorted.bam"
+
+    log: "log/bamsort/{sample}/{sample}_sort.log"
+
+    benchmark: "benchmarks/{sample}_bam_sort_benchmark.txt"
+
+    shell: 
+      r"""
+      module load SAMtools/1.3.1-foss-2015a
+      (samtools sort -o {output} {input}) &> {log}
+      
+      """
+rule ReadDuplicates:
+    input: "output/bam/{sample}/{sample}_sorted.bam"
+
+    output: 
+        RD = "output/bam/{sample}/{sample}_dedup.bam",
+        MET = "log/read_dup/{sample}/metrics_{sample}.txt"
+
+    log: "log/read_dup/{sample}/{sample}_rmdup_new.log"
+
+    benchmark: "benchmarks/{sample}_ReadDuplicates_benchmark.txt"
+
+    shell:
+      r"""
+      (java -jar /fast/users/stincy/scratch/miniconda/pkgs/picard-2.11.0-py35_0/share/picard-2.11.0-0/picard.jar MarkDuplicates \
+             I={input} \
+             O={output.RD} \
+             M={output.MET} \
+             AS=true) &> {log}
+      """
+
+rule bamindexInitial:
+    input: "output/bam/{sample}/{sample}_sorted.bam"
+
+    output: "output/bam/{sample}/{sample}_sorted.bam.bai"
+
+    log: "log/BAI/{sample}/{sample}_baifile.log"
+
+    benchmark: "benchmarks/{sample}_BAM_index_benchmark.txt"
+
+    shell:
+      r"""
+      module load SAMtools/1.3.1-foss-2015a
+      samtools index -b {input} &> {log}
+      
+      """
+rule bamindexInitial_dedup:
+    input: "output/bam/{sample}/{sample}_dedup.bam"
+
+    output: "output/bam/{sample}/{sample}_dedup.bam.bai"
+
+    log: "log/BAI/{sample}/{sample}_baifile_dedup.log"
+
+    shell:
+      r"""
+      module load SAMtools/1.3.1-foss-2015a
+      samtools index -b {input} &> {log}
+
+      """
+
+
+rule indelReAlignerTargetCreator:
+    input: 
+        bam = "output/bam/{sample}/{sample}_dedup.bam",
+        bai = "output/bam/{sample}/{sample}_dedup.bam.bai"
+    
+    params: 
+        REF = "/fast/projects/cubit/current/static_data/reference/GRCh37/hs37d5/hs37d5.fa"
+
+    output: "output/realin_inter/ReAlignerTaretCreator_{sample}.intervals"
+  
+    log: "log/ReAlignerTaretCreator/{sample}/ReAlignerTaretCreator_{sample}.log"
+
+    benchmark: "benchmarks/{sample}_indelRealignerTargetCreator_benchmark.txt"
+
+    shell:
+      r"""
+      
+      (java -jar /fast/projects/ngs_sers/Packages/tools/GATK3.7/GenomeAnalysisTK.jar \
+           -T RealignerTargetCreator \
+           -R {params.REF} \
+           -I {input.bam} \
+           -o {output}) &> {log}
+      """
+
+rule indelReAligner:
+    input:
+        IR = "output/bam/{sample}/{sample}_dedup.bam",
+        TI= "output/realin_inter/ReAlignerTaretCreator_{sample}.intervals"
+    params: 
+        reference= "/fast/projects/cubit/current/static_data/reference/GRCh37/hs37d5/hs37d5.fa",
+        #TI= "output/realin_inter/ReAlignerTaretCreator_{sample}.intervals"
+
+    output: "output/bam/{sample}/{sample}_realigned.bam"
+
+    log: "log/realign/{sample}/{sample}_realignment.log"
+
+    benchmark: "benchmarks/{sample}_indel_ReAligner_benchmark.txt"
+
+    shell:
+      r"""
+      (java -jar /fast/projects/ngs_sers/Packages/tools/GATK3.7/GenomeAnalysisTK.jar \
+           -T IndelRealigner \
+           -R {params.reference} \
+           -I {input.IR} \
+           -targetIntervals {input.TI} \
+           -o {output}) &> {log}
+      """ 
+
+rule BQSR: 
+    input: "output/bam/{sample}/{sample}_realigned.bam"
+
+    params: 
+        REF = "/fast/projects/cubit/current/static_data/reference/GRCh37/hs37d5/hs37d5.fa",
+	KWN = "/fast/projects/cubit/current/static_data/db/dbSNP/b147/GRCh37/All_20160408.vcf.gz"
+    
+    output: "output/BQSR/{sample}/{sample}_CovarTable_Recal.table"
+
+    log: "log/BQSR/{sample}/{sample}_BQSR.log"
+
+    benchmark: "benchmarks/{sample}_BQSR_benchmark.txt"
+
+    shell:
+      r"""
+      (java -jar /fast/projects/ngs_sers/Packages/tools/GATK3.7/GenomeAnalysisTK.jar \
+           -T BaseRecalibrator \
+           -R {params.REF} \
+           -I {input} \
+           -knownSites {params.KWN} \
+           -o {output}) &> {log}
+      """
+
+rule RecalibratedBam: 
+    input: 
+        RE = "output/bam/{sample}/{sample}_realigned.bam",
+        BQSR = "output/BQSR/{sample}/{sample}_CovarTable_Recal.table"
+
+    params:
+        REF = "/fast/projects/cubit/current/static_data/reference/GRCh37/hs37d5/hs37d5.fa"
+	#BQSR = "output/BQSR/{sample}/{sample}_CovarTable_Recal.grp"
+
+    output: "output/bam/{sample}/{sample}_recalibrated.bam"        
+
+    log: "log/Recalibrate/{sample}/{sample}_Recalibrated.log"
+    
+    benchmark: "benchmarks/{sample}_Recalibrated_Bam_benchmark.txt"
+
+    shell:
+      r"""
+
+      (java -jar /fast/projects/ngs_sers/Packages/tools/GATK3.7/GenomeAnalysisTK.jar \
+           -T PrintReads \
+           -R {params.REF} \
+           -I {input.RE} \
+           -BQSR {input.BQSR} \
+           -o {output}) &> {log}
+      """
---
+++ Filteration_Strelka_AnnovarSNV_Input.sh
@@ -0,0 +1,248 @@
+SAMPLES = ["PNET10P", "PNET19P", "PNET8P", "PNET20P", "PNET21P", "PNET22P", "PNET23P", "PNET24P"]
+
+rule all:
+	input:
+          #expand("Data/CommonAlteration/InputFiles/{sample}/somatic.snvs.vcf", sample = SAMPLES),
+          #expand("Data/CommonAlteration/InputFiles/{sample}/somatic.indels.vcf", sample = SAMPLES),
+          #expand("output/Strelka/Results/{sample}/results/variants/somatic.snvsPASS.vcf", sample = SAMPLES),
+          #expand("output/Strelka/Results/{sample}/results/variants/somatic.indelsPASS.vcf",sample = SAMPLES),
+          expand("Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm.vcf", sample = SAMPLES),
+          expand("Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm_1000G.vcf", sample = SAMPLES),
+          expand("Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm_1000G_1000GFilter.vcf", sample = SAMPLES),
+          expand("Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_w_AF.vcf", sample = SAMPLES),
+          expand("Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_temp.vcf", sample = SAMPLES),
+	  expand("Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_biasRemoval.vcf", sample = SAMPLES),
+	  expand("Data/StrelkaVariants/Filteration/new/{sample}/{sample}_StrelkaSNV.vcf", sample = SAMPLES),
+	  expand("Data/StrelkaVariants/Filteration/new/{sample}/{sample}_StrelkaSNV.vcf.gz", sample = SAMPLES),
+	  expand("Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_InitialResults.tsv", sample = SAMPLES),
+	  expand("Data/StrelkaVariants/Filteration/new/{sample}/{sample}_WESNoHeaderSNV.tsv", sample = SAMPLES),
+	  expand("Data/StrelkaVariants/Filteration/new/{sample}/{sample}_WES_FormattedSNV.tsv", sample = SAMPLES)
+
+rule unzipSNVStrelka:
+        input: "output/Strelka/Results/{sample}/results/variants/somatic.snvs.vcf.gz"
+
+        output: "Data/CommonAlteration/InputFiles/{sample}/somatic.snvs.vcf"
+
+        log: "log/unzip/{sample}snv_strelka_unzip.log"
+
+        shell:
+           r"""
+               	module load SAMtools/1.3.1-foss-2015a
+                (bgzip -d {input} > {output}) &> {log}
+           """
+
+rule unzipIndelStrelka:
+        input: "output/Strelka/Results/{sample}/results/variants/somatic.indels.vcf.gz"
+
+        output: "Data/CommonAlteration/InputFiles/{sample}/somatic.indels.vcf"
+
+        log: "log/unzip/{sample}indel_strelka_unzip.log"
+
+        shell:
+           r"""
+               	module load SAMtools/1.3.1-foss-2015a
+                (bgzip -d {input} > {output}) &> {log}
+           """
+
+rule PassStrelkaSNV:
+        input: "output/Strelka/Results/{sample}/results/variants/somatic.snvs.vcf"
+
+        output: "output/Strelka/Results/{sample}/results/variants/somatic.snvsPASS.vcf"
+
+        log: "log/PASS/{sample}somatic.snvsPASS.log"
+
+        shell:
+          r"""
+              	(java -Xmx10g -jar /fast/projects/ngs_sers/Packages/tools/snpEff_latest-version/snpEff/SnpSift.jar \
+                        filter "( na FILTER ) | (FILTER = 'PASS')" -f {input} > {output}) &> {log}
+          """
+rule PassStrelkaINDEL:
+        input: "output/Strelka/Results/{sample}/results/variants/somatic.indels.vcf"
+
+        output: "output/Strelka/Results/{sample}/results/variants/somatic.indelsPASS.vcf"
+
+        log: "log/PASS/{sample}somatic.INDELPASS.log"
+
+        shell:
+          r"""
+                (java -Xmx10g -jar /fast/projects/ngs_sers/Packages/tools/snpEff_latest-version/snpEff/SnpSift.jar \
+                        filter "( na FILTER ) | (FILTER = 'PASS')" -f {input} > {output}) &> {log}
+          """
+
+rule SNVSegDup:
+        input: VCF="output/Strelka/Results/{sample}/results/variants/somatic.snvsPASS.vcf"
+
+        output: "Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm.vcf"
+
+        params: "/fast/projects/ngs_sers/Packages/Regions/Seg_Dup/GRCh37_SegDuplication_sort.bed"
+
+        log: "log/SegDup/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm.vcf.log"
+
+        shell:
+          r"""
+                module load BEDTools/2.24.0-foss-2015a
+                (bedtools intersect -v -header -a {input.VCF} -b {params} > {output}) &> {log}
+
+          """
+
+rule SNVAnn1000G:
+        input:
+          "Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm.vcf"
+
+        output:
+          "Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm_1000G.vcf"
+
+        params: "/fast/projects/ngs_sers/Packages/Regions/1000G/1000G.EUR_AF.vcf.gz"
+          #"/fast/projects/ngs_sers/Packages/Regions/WES/1000G_EUR_AF_Filter.vcf.gz"
+
+        log: 
+          "log/StrelkaVariants/Filteration/{sample}/{sample}_1000G.log"
+
+        shell:
+          r"""
+              	(java -Xmx10g -jar /fast/projects/ngs_sers/Packages/tools/snpEff_latest-version/snpEff/SnpSift.jar \
+                        annotate {params} {input} > {output}) &> {log}
+
+          """
+rule SNVGFilter:
+        input:
+          "Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm_1000G.vcf"
+
+        output:
+          "Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm_1000G_1000GFilter.vcf"
+
+        log:
+          "log/StrelkaVariants/Filteration/{sample}/{sample}_1000Gfilter.log"
+
+        shell:
+          r"""
+              	(java -Xmx10g -jar /fast/projects/ngs_sers/Packages/tools/snpEff_latest-version/snpEff/SnpSift.jar \
+                        filter -n "(EUR_AF > 0.05)" {input} > {output}) &> {log}
+          """
+
+rule SNVAdAlleleFreqRef:
+        input:
+          "Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_SegDup_rm_1000G_1000GFilter.vcf"
+
+        output:
+          "Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_w_AF.vcf"
+
+        log:
+          "log/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_w_AF.log"
+
+        script:
+          "StrelkaAF.py"
+
+rule addHeader:
+        input:
+          AVCF="/fast/projects/ngs_sers/2017_WES_Analysis/PNET/test.vcf",
+          BVCF="Data/StrelkaVariants/Filteration/{sample}/{sample}_Strelka_w_AF.vcf"
+        output:
+          Header="Data/StrelkaVariants/Filteration/{sample}/{sample}_header.vcf",
+          intersectfile="Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_temp.vcf"
+
+        log: "log/Strelka/Header/{sample}_Header.log"
+
+        shell:
+          r"""
+                grep -P '^#' {input.AVCF} > {output.Header}
+                (sed r {output.Header} {input.BVCF} > {output.intersectfile}) &> {log}
+
+          """
+
+rule SNVDKFZBiasFilter:
+        input:
+          bam = "output/bam/RecalibratedBamTumor/{sample}_recalibrated.bam",
+          vcf = "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_temp.vcf"
+
+        params:
+          ref='/fast/projects/cubit/current/static_data/reference/GRCh37/hs37d5/hs37d5.fa',
+          tempFolder='Data/tmp'
+        output:
+          vcf = 'Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_biasRemoval.vcf'
+
+        log: 'log/Strelka/dkfzbiasfilter/{sample}.log'
+
+        shell:
+          r"""
+              	(/fast/users/simont_c/scratch/miniconda3/envs/py27/bin/dkfzbiasfilter.py \
+                        --tempFolder {params.tempFolder} \
+                        --writeQC \
+                        {input.vcf} {input.bam} {params.ref} {output.vcf}) &> {log}
+          """
+
+rule SNVfinalFilter:
+        input: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_biasRemoval.vcf"
+
+        output: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_StrelkaSNV.vcf"
+
+        log: "log/Strelka/{sample}_Strelka_InitialResults.log"
+
+        shell:
+          r"""
+              	(java -Xmx10g -jar /fast/projects/ngs_sers/Packages/tools/snpEff_latest-version/snpEff/SnpSift.jar \
+                        filter "( na FILTER ) | (FILTER = 'PASS')" -f {input} > {output}) &> {log}
+          """
+
+
+rule ZipInx:
+	input: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_StrelkaSNV.vcf"
+
+        output: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_StrelkaSNV.vcf.gz"
+
+        log: "log/Strelka/ZipInx/{sample}_WES.vcf.log"
+
+        shell:
+          r"""
+              	module load SAMtools/1.3.1-foss-2015a
+                (bgzip -c {input} > {output}) &> {log}
+
+          """
+
+rule FriendlyFormall:
+        input: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_StrelkaSNV.vcf"
+
+        output: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_InitialResults.tsv"
+
+        log: "log/Strelka/{sample}_csv.log"
+
+
+        shell:
+          r"""
+              	(cat {input} \
+                | /fast/projects/ngs_sers/Packages/tools/snpEff_latest-version/snpEff/scripts/vcfEffOnePerLine.pl \
+                | java -Xmx10g -jar /fast/projects/ngs_sers/Packages/tools/snpEff_latest-version/snpEff/SnpSift.jar \
+                        extractFields - -s "," -e "-" CHROM POS ID REF ALT QUAL FILTER \
+                        "GEN[*].REF_AF" "GEN[*].ALT_AF" > {output}) &> {log}
+          """
+
+
+
+rule TSVtoBed:
+	input: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_Strelka_InitialResults.tsv"
+
+        output: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_WESNoHeaderSNV.tsv"
+
+        log: "log/filteration/Strelka/{sample}_tsvnoheader.log"
+
+        shell:
+          r"""
+              	(awk -F' ' '{{if (NR!=1) {{print}}}}' {input} > {output}) &> {log}
+
+          """
+
+rule NewTSVtoBed:
+        input: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_WESNoHeaderSNV.tsv"
+
+        output: "Data/StrelkaVariants/Filteration/new/{sample}/{sample}_WES_FormattedSNV.tsv"
+
+        log: "log/filteration/Strelka/{sample}_tsvformated.log"
+
+        shell:
+          r"""
+
+              	(awk -F'\t' '{{printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$2,$4,$5,$7,$8,$9)}}' < {input} > {output}) &> {log}
+
+          """
+
+
